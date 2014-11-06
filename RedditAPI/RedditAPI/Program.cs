984871b0ee857feb45b3666f237440aa3f225e93
Joe <joe.m.cullen1990@gmail.com>
2014-11-06 01:58:14 +0000

    Change back to my test solution
    Use RedditSharp DLL as a reference rather than adding to it at this time

---
+++ RedditAPI/RedditAPI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RedditSharp;
+
+namespace RedditAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reddit = new Reddit();
+            var user = reddit.LogIn("Paramnesia1", "taurine1");
+            var subreddit = reddit.GetSubreddit("/r/formula1");
+            Console.Write(subreddit.Description);
+            Console.Read();
+
+        }
+    }
+}
